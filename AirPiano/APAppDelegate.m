19a24cfc8ea64b9ca1e0bb923e78a285b60b602a
Aanand Prasad <aanand.prasad@gmail.com>
2013-02-26 19:26:34 +0000

    Create two bounding boxes and detect whether fingers are in either of them

--- AirPiano/APAppDelegate.m
+++ AirPiano/APAppDelegate.m
@@ -10,20 +10,53 @@
 #import "FingertipListener.h"
 
 @interface APAppDelegate () <FingertipListenerDelegate>
+
+@property (strong, nonatomic) NSArray *keys;
+@property (strong, nonatomic) NSArray *yRange;
+
 @end
 
 @implementation APAppDelegate
 
+@synthesize keys, yRange;
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    FingertipListener *listener = [[FingertipListener alloc]init];
+	self.keys = @[
+  @{ @"x": @[@-100, @0], @"note": @0 },
+  @{ @"x": @[@0, @100], @"note": @1 }
+  ];
+
+    self.yRange = @[@100, @200];
+	
+	FingertipListener *listener = [[FingertipListener alloc]init];
 	listener.delegate = self;
     [listener run];
 }
 
 -(void)fingertipListener:(FingertipListener *)listener didDetectFingertipsAtPositions:(NSArray *)positions
 {
-	NSLog(@"fingertips: %@", [positions componentsJoinedByString:@", "]);
+	NSMutableSet *notes = [NSMutableSet set];
+	
+	NSNumber *yMin = [yRange objectAtIndex:0];
+	NSNumber *yMax = [yRange objectAtIndex:1];
+	
+	for (NSDictionary *key in self.keys) {
+		for (LeapVector *position in positions) {
+			NSArray *xRange = [key objectForKey:@"x"];
+			NSNumber *xMin = [xRange objectAtIndex:0];
+			NSNumber *xMax = [xRange objectAtIndex:1];
+
+			if (position.x >= xMin.floatValue && position.x <= xMax.floatValue &&
+				position.y >= yMin.floatValue && position.y <= yMax.floatValue) {
+				[notes addObject:[key objectForKey:@"note"]];
+			}
+		}
+	}
+	
+	if (notes.count > 0) {
+		NSLog(@"notes = %@", notes);
+	}
 }
 
 @end
